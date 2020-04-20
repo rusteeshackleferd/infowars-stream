7a3f7f62c8d28885d04d15696ceed38626c7e2de
rustee <rustedshacklesford@gmail.com>
2020-04-20 13:23:12 -0400

    redirect to ssl

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(request, response){
+  if(!request.secure){
+    response.redirect("https://" + request.headers.host + request.url);
+  }
+});
 
 app.use('/', indexRouter);
 
