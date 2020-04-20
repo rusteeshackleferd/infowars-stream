0fa79b6027bd291ccc5a4d84c1cafb79099fd225
rustee <rustedshacklesford@gmail.com>
2020-04-20 13:32:21 -0400

    more ssl changes

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.enable("trust proxy");
 app.use(function(request, response){
   if(request.protocol === 'http'){
     response.redirect("https://" + request.headers.host + request.url);
