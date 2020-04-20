fdfba7ec5d926bb066ac7c31a592738aaf8c1780
rustee <rustedshacklesford@gmail.com>
2020-04-20 13:42:52 -0400

    fix quote characters

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var sslRedirect = require(‘heroku-ssl-redirect’);
+var sslRedirect = require('heroku-ssl-redirect');
 
 var app = express();
 
