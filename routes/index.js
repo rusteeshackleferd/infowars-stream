4f3c36c50b1951feb893688af2b998ef00e9696f
rustee <rustedshacklesford@gmail.com>
2020-04-21 22:52:10 -0400

    alex's servers are terrible sometimes, so let's hardcode values instead of asking

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,8 @@ var HTMLParser = require('node-html-parser');
 
 /* GET home page. */
 router.get('/', async(req, res, next) => {
-  let response = await got('https://banned.video/channel/the-alex-jones-show');
-  let channel_page = HTMLParser.parse(response.body, {script: true});
-  let channelInfo = JSON.parse(channel_page.querySelector('#__NEXT_DATA__').rawText);
-  let channelId = channelInfo.props.pageProps.channel._id;
-  let streamUrl = channelInfo.props.pageProps.channel.liveStreamVideo.streamUrl;
+  let channelId = '5b885d33e6646a0015a6fa2d';
+  let streamUrl = 'https://infostream.secure.footprint.net/hls-live/infostream-infostream/_definst_/live.m3u8';
   res.render('index', { title: 'Data Skirmish - Ad-lite, Reality Adjacent', channelId, streamUrl });
 });
 
