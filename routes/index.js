c71941795a9826395a98c2b4a8a2d528d12b0b33
rustee <rustedshacklesford@gmail.com>
2020-07-27 14:10:45 -0400

    change the stream to match the new one

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var HTMLParser = require('node-html-parser');
 /* GET home page. */
 router.get('/', async(req, res, next) => {
   let channelId = '5b885d33e6646a0015a6fa2d';
-  let streamUrl = 'https://infostream.secure.footprint.net/hls-live/infostream-infostream/_definst_/live.m3u8';
+  let streamUrl = 'https://freespeech.akamaized.net/hls/live/2016712/live2/playlist.m3u8';
   res.render('index', { title: 'Data Skirmish - Ad-lite, Reality Adjacent', channelId, streamUrl });
 });
 
